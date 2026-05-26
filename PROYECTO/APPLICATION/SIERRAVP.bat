b642a418f1720d807deb60426db09503302f86d7
DevMemo26 <guillermo.ramosu@unmsm.edu.pe>
2026-05-26 01:29:58 -0500

    Add files via upload

---
+++ PROYECTO/APPLICATION/SIERRAVP.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Java\jdk-21.0.11\bin\java.exe" -cp SIERRAVP.jar com.killa.sierravp.client.SIERRAVP
+pause
---
+++ SERVIDOR/APPLICATION/Update-now.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Java\jdk-21\bin\java.exe" -cp SIERRAVP.jar com.killa.sierravp.client.Update now
+pause
